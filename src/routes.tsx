14f05651635cf79f8aef1e59fad3aee09793a062
Azhaguraju Pandi <azhaguking@gmail.com>
2024-12-29 00:28:43 +0530

    routes added

---
+++ src/routes.tsx
@@ -0,0 +1,20 @@
+import { Route, Routes } from "react-router-dom";
+
+// Route Views
+import TabPanel from './views/employeeTab'
+import ViewEmployeeTable from './../src/views/viewEmployeeTable';
+import AddEmployeeTable from './../src/views/addEmployee';
+import EditEmployeeTable from './../src/views/editEmployee';
+
+export default function RouterPage() {
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<TabPanel />} />
+        <Route path="/viewEmployee" element={<ViewEmployeeTable />} />
+        <Route path="/addEmployee" element={<AddEmployeeTable />} />
+        <Route path="/editEmployee" element={<EditEmployeeTable />} />
+      </Routes>
+    </div>
+  )
+}
